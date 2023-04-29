716b3250cd3aff80a7c9db695483b00129885e13
TatianaKedrova <127103022+TatianaKedrova@users.noreply.github.com>
2023-04-29 17:35:28 +0200

    Set the time period AM/PM, Set the 12-hour clock format, Add the 0 for the time values lower than 10 (Javascript)

--- main.js
+++ main.js
@@ -8,6 +8,32 @@ function clock() {
     var seconds = today.getSeconds();
     let period = "AM";
 
+//Set the time period AM/PM    
+
+   if(hours >= 12){
+      period = "PM";
+    }
+
+//Set the 12-hour clock format
+    hours = hours > 12 ? hours % 12 : hours;
+
+
+//Add the "0" for the time values Lower than 10
+    if(hours < 10){
+        hours = "0" + hours;
+    }
+
+    if(minutes< 10){
+        minutes = "0" + minutes;
+    }
+
+    if(seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+
+
+
     document.querySelector(".hours").innerHTML = hours;
     document.querySelector(".minutes").innerHTML = minutes;
     document.querySelector(".seconds").innerHTML = seconds;
